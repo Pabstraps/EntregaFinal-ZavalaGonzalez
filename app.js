5f13299484c99877e920a06e0bbea6159566659e
Pablo Zavala Gonzalez <Pablozg24@gmail.com>
2023-05-17 19:47:51 -0600

    Update app.js
    Tercer commit para la entrega final,
    
    se actualiza el footer y se agregan eventos

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cards = document.getElementById(`cards`)
 const items = document.getElementById(`items`)
 const footer = document.getElementById(`footer`)
 const templateCard = document.getElementById(`template-card`).content
-const templatefooter = document.getElementById(`template-footer`).content
+const templateFooter = document.getElementById(`template-footer`).content
 const templateCarrito = document.getElementById(`template-carrito`).content
 const fragment = document.createDocumentFragment()
 
@@ -13,12 +13,19 @@ const fragment = document.createDocumentFragment()
 let carrito = {}
 
 
+//Se declaran los eventos
+
 document.addEventListener(`DOMContentLoaded`, () => {
     fetchData()
 })
 cards.addEventListener(`click`, e => {
     addCarrito(e)
 })
+
+items.addEventListener(`click`, e => {
+    btnAccion(e)
+})
+
 
 //Se hace el Fetch del archivo JSON
 
@@ -31,7 +38,9 @@ const fetchData = async () => {
 
     } catch (error) {
         console.log (error)
+
     }
+
 }
 
 
@@ -61,8 +70,8 @@ const addCarrito = e => {
 const setCarrito = objeto => {
 
     const producto = {
-        id: objeto.querySelector(`.btn-dark`).textContent,
-        tittle: objeto.querySelector(`h5`).textContent,
+        id: objeto.querySelector(`.btn-dark`).dataset.id,
+        title: objeto.querySelector(`h5`).textContent,
         precio: objeto.querySelector(`p`).textContent,
         cantidad: 1
     }
@@ -78,6 +87,53 @@ const setCarrito = objeto => {
 //Se imprime el carrito
 
 const pintarCarrito = () => {
-    console.log (carrito)
+
+    items.innerHTML = ''
+    Object.values(carrito).forEach(producto =>{
+        templateCarrito.querySelector(`th`).textContent = producto.id
+        templateCarrito.querySelectorAll(`td`)[0].textContent = producto.title
+        templateCarrito.querySelectorAll(`td`)[1].textContent = producto.cantidad
+        templateCarrito.querySelector(`.btn-info`).dataset.id = producto.id
+        templateCarrito.querySelector(`.btn-danger`).dataset.id = producto.id
+        templateCarrito.querySelector(`span`).textContent = producto.cantidad * producto.precio
+        const clone = templateCarrito.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
+
+    pintarFooter()
+
 }
 
+const pintarFooter = () => {
+    footer.innerHTML = ''
+    if (Object.keys(carrito).length ===0){
+        footer.innerHTML = `
+        <th scope="row" colspan="5">Carrito vacío - comience a comprar!</th>
+        `
+        return
+    }
+
+    const nCantidad = Object.values(carrito).reduce((acc,{cantidad}) => acc + cantidad, 0)
+    const nPrecio = Object.values(carrito).reduce((acc,{cantidad,precio}) => acc + cantidad * precio, 0)
+
+    templateFooter.querySelectorAll(`td`)[0].textContent = nCantidad
+    templateFooter.querySelector(`span`).textContent = nPrecio
+
+    const clone = templateFooter.cloneNode(true)
+    fragment.appendChild(clone)
+    footer.appendChild(fragment)
+
+    const btnVaciar = document.getElementById(`vaciar-carrito`)
+    btnVaciar.addEventListener(`click`, () => {
+        carrito = {}
+
+        pintarCarrito()
+
+    })
+
+}
+
+const btnAccion = e => {
+    console.log(e.target)
+}
